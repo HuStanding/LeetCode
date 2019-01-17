de68a0e611309e63ee32ffb5eaa46fc75dda5c67
HustElvan <1099270667@qq.com>
2019-01-17 16:45:20 +0800

    数组(含重复元素)的不重复子集

--- src/Code_90_Subsets2.java
+++ src/Code_90_Subsets2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author huzhu
  * @date created in 2019-01-17 16:34
@@ -5,8 +9,28 @@
  */
 public class Code_90_Subsets2 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(subsetsWithDup(new int[]{1,2,2}));
     }
 
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums,visited,new ArrayList<>(),0,res);
+        return res;
+    }
 
+    public static void dfs(int[] nums,boolean[] visited,List<Integer> list,int start,List<List<Integer>> res){
+        res.add(new ArrayList<>(list));
+        for(int i = start;i < nums.length;i++){
+            if(i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]){
+                continue;
+            }
+            list.add(nums[i]);
+            visited[i] = true;
+            dfs(nums,visited,list,i + 1,res);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }
 }
