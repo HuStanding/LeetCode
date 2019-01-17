7456b3e3754991bfb4817f860b6e3ee9716f7ba2
HustElvan <1099270667@qq.com>
2019-01-17 17:54:34 +0800

    数组的子集

--- src/Code_78_Subsets.java
+++ src/Code_78_Subsets.java
@@ -4,11 +4,12 @@ import java.util.List;
 /**
  * @author huzhu
  * @date created in 2019-01-17 16:04
- * @explaination 数组的子集（不包含重复子集）
+ * @explaination 数组的子集
  */
 public class Code_78_Subsets {
     public static void main(String[] args) {
         System.out.println(subsets(new int[]{1,2,3}));
+
     }
 
     public static List<List<Integer>> subsets(int[] nums) {
